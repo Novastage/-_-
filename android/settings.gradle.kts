e8e4ce732d669c0c149842c8c164b87f16a585ff
Novastage <djmix9999@gmail.com>
2026-09-16 08:59:23 +0900

    Add Android Simple Core project settings

---
+++ android/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "NOVA_RED_LINE_MOBILE"
+include(":app")
